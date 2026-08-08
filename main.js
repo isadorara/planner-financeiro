b107544c5098c5b14eb9fe97e2dbb5533930dd78
isadorara <isadora_caraujo@outlook.com>
2026-08-07 22:04:47 -0300

    Add table and input options

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-let transacao = []
-let categorias = ["Alimentação", "Transporte", "Saúde", "Lazer", "Moradia", "Assinatura", "Outros"]
+const tabela = document.getElementById('tableBody')
 
+function cadastrarSaida() {
+    const saida = document.getElementById('input_saida');
+    const valor = document.getElementById('input_valor');
+    const categoria = document.getElementById('input_categoria');
+    const data = document.getElementById('input_data');
+
+    const novaLinha = `
+        <tr>
+            <td>${saida.value}</td>
+            <td>R$${valor.value}</td>
+            <td>${categoria.value}</td>
+            <td>${data.value}</td>
+        </tr>
+    `;
+
+
+    console.log('Saída cadastrada!');
+
+    tabela.insertAdjacentHTML('beforeend', novaLinha);
+}
